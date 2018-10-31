1473bac26bebae940a5c1be62acc27f2323aaeff
gohuygo <huy@gohuygo.com>
2018-10-31 01:15:00 -0700

    the 4 spacing life

--- Ballot.sol
+++ Ballot.sol
@@ -1,78 +1,78 @@
 pragma solidity ^0.4.24;
 
 contract Ballot {
-  string public vote;
+    string public vote;
 
-  constructor (string _vote) public {
-    vote = _vote;
-  }
+    constructor (string _vote) public {
+        vote = _vote;
+    }
 
-  function contractAddress() public view returns (address) {
-    return address(this);
-  }
+    function contractAddress() public view returns (address) {
+        return address(this);
+    }
 }
 
 contract BallotFactory {
-  address owner;
-  bool isOpen;
-  uint64 maxVote;
-  string public winner;
-  string[] candidates;
-  mapping(address => address) voterContracts;
-  mapping(string => uint) candidateVotes;
+    address owner;
+    bool isOpen;
+    uint64 maxVote;
+    string public winner;
+    string[] candidates;
+    mapping(address => address) voterContracts;
+    mapping(string => uint) candidateVotes;
 
-  event newVote(address owner, address ballotAddress);
+    event newVote(address owner, address ballotAddress);
 
-  modifier onlyOnce {
-    require(voterContracts[msg.sender] == 0);
-    _;
-  }
+    modifier onlyOnce {
+        require(voterContracts[msg.sender] == 0);
+        _;
+    }
 
-  modifier onlyOpen {
-    require(isOpen);
-    _;
-  }
+    modifier onlyOpen {
+        require(isOpen);
+        _;
+    }
 
-  modifier onlyClosed {
-    require(!isOpen);
-    _;
-  }
+    modifier onlyClosed {
+        require(!isOpen);
+        _;
+    }
 
-  modifier onlyOwner {
-    require(msg.sender == owner);
-    _;
-  }
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
-  constructor (uint64 _maxVote) public {
-      owner = msg.sender;
-      maxVote = _maxVote;
-      isOpen = true;
-  }
+    constructor (uint64 _maxVote) public {
+        owner = msg.sender;
+        maxVote = _maxVote;
+        isOpen = true;
+    }
 
-  function withdraw() public onlyOwner returns(bool) {
-    owner.transfer(address(this).balance);
-    return true;
-  }
+    function withdraw() public onlyOwner returns(bool) {
+        owner.transfer(address(this).balance);
+        return true;
+    }
 
-  function closeBallot() public onlyOwner onlyOpen payable returns(bool){
-    // Prevent accidental closingn of ballot
-    require(msg.value >= 1 ether);
+    function closeBallot() public onlyOwner onlyOpen payable returns(bool){
+        // Prevent accidental closingn of ballot
+        require(msg.value >= 1 ether);
 
-    isOpen = false;
+        isOpen = false;
 
-    return true;
-  }
+        return true;
+    }
 
-  function createVote(string _vote) public onlyOnce onlyOpen returns(bool) {
-    Ballot ballot = new Ballot(_vote);
-    voterContracts[msg.sender] = ballot.contractAddress();
-    candidateVotes[_vote] += 1;
-    candidates.push(_vote);
+    function createVote(string _vote) public onlyOnce onlyOpen returns(bool) {
+        Ballot ballot = new Ballot(_vote);
+        voterContracts[msg.sender] = ballot.contractAddress();
+        candidateVotes[_vote] += 1;
+        candidates.push(_vote);
 
-    emit newVote(msg.sender, voterContracts[msg.sender]);
+        emit newVote(msg.sender, voterContracts[msg.sender]);
 
-    return true;
-  }
+        return true;
+    }
 
   function getWinner() onlyOwner onlyClosed public returns (string) {
     uint count;
@@ -85,14 +85,14 @@ contract BallotFactory {
             winner = candidates[i];
             count = currentCount;
         }else if(currentCount == count){
-          tied = true;
+            tied = true;
         }
     }
 
     if(tied){
-      return 'tied';
+        return 'tied';
     }else{
-      return winner;
+        return winner;
     }
   }
 }
