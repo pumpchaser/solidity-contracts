f5e33352b09cab8d9437439e3b2d6a3cb9647de0
gohuygo <huy@gohuygo.com>
2018-11-01 19:27:07 -0700

    clean safe math

--- Safemath.sol
+++ Safemath.sol
@@ -4,25 +4,17 @@ library Safemath {
   function addition(uint _a, uint _b) public pure returns (uint){
     // require(2**256 - 1 - _a > _b);
     uint sum = _a + _b;
-
     require(sum >= _a && sum >= _b);
-    return _a + _b;
+    return sum;
   }
 
   function subtraction(uint _a, uint _b) public pure returns (uint){
     require( _a >= _b );
-
     return _a - _b;
   }
 
   function multiplication(uint _a, uint _b) public pure returns (uint){
-    if (_a == 0 || _b == 0){
-        return 0;
-    }
-
     uint product = _a * _b;
-    // uint max = 2**256 - 1;
-    // require((max / _a) > _b);
     require(product/_a == _b);
 
     return product;
